375f593101452b9d35d2eac660c2d53ae39a7dcc
JordanZunde <jordanzunde@gmail.com>
2023-10-13 19:46:25 +0200

    Tally

--- Assets/Scripts/Managers/OrbTally.cs
+++ Assets/Scripts/Managers/OrbTally.cs
@@ -5,10 +5,24 @@ using UnityEngine;
 public class OrbTally : MonoBehaviour
 {
     public int heldOrbs, maxOrbs = 6;
+   public QuestionManager questionManager;
     // Start is called before the first frame update
     void Start()
     {
         heldOrbs = 0;
+        questionManager = GetComponent<QuestionManager>();
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        for (int i = 0; i < questionManager.questions.Length; i++)
+        {
+            if (collision.gameObject == questionManager.questions[i].gameObject)
+            {
+                Time.timeScale = 0f;
+                questionManager.qCanvases[i].SetActive(true);
+            }
+        }
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
