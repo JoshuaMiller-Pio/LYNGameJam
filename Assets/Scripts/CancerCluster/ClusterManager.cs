16a45fab9364dd8f12b89820bf2a7e1cb7e4431b
JordanZunde <jordanzunde@gmail.com>
2023-10-26 11:55:56 +0200

    VictoryConditionAndCanvas

--- Assets/Scripts/CancerCluster/ClusterManager.cs
+++ Assets/Scripts/CancerCluster/ClusterManager.cs
@@ -1,12 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ClusterManager : MonoBehaviour
 {
     public GameObject player;
     public OrbTally playerOrbTally;
     public GameObject[] tumours = new GameObject[6];
+    public Canvas win;
     // Start is called before the first frame update
     void Start()
     {
@@ -20,6 +22,11 @@ public class ClusterManager : MonoBehaviour
             for (int i = 0; i < playerOrbTally.heldOrbs; i++)
             {
                 tumours[i].SetActive(false);
+                if(i >= 6)
+                {
+                    win.gameObject.SetActive(true);
+                    Time.timeScale = 0;
+                }
             }
         }
     }
