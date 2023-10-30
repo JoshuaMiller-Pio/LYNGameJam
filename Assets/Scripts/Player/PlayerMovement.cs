2ccb7f5832e132076cd08d69c6ffd5697e0b1004
Joshua Miller-Pio <joshuapio585@gmail.com>
2023-10-30 18:25:29 +0200

    final push

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -1,8 +1,6 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
-using static UnityEditor.Timeline.TimelinePlaybackControls;
 
 public class PlayerMovement : MonoBehaviour
 {
